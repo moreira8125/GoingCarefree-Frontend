37637e29239ec358667e823e6b310c19bc77604c
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2024-02-17 17:12:08 +0100

    changes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import "../styles/contacts.css"
 
 function Contacts() {
   return (
-    <div className='contactContainer'>
-     <h1>Contact Us</h1>
-     <form>
-      <label><p>Name:</p>
-        <input type="text" />
-      </label>
-      <label> <p>Email:</p>
-        <input type="text" />
-      </label>
-      <label id='labelMessage'> <p>How can we help?:</p>
-     
-        <textarea name="" id="" cols="80" rows="10"></textarea>
-        
-      </label>
-  
-      <div className='buttonContainer'>
-  <button>Send</button>
-</div>
-
-     </form>
+    <div>
+      
     </div>
   )
 }
