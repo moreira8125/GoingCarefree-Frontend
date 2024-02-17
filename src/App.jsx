335291d4724be8bbfb169368862b85021d405d14
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2024-02-17 11:00:20 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar"
+import Homepage from "./pages/Homepage"
+import Destinations from "./pages/Destinations"
+import DestinationDetail from "./pages/DestinationDetail"
+import Reviews from "./pages/Reviews"
+import Admin from "./pages/Admin"
+import Contacts from "./pages/Contacts"
+import {Routes, Route} from 'react-router-dom'
+
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Homepage/>}/>
+    <Route path="/destinations" element={<Destinations/>}/>
+    <Route path="/destinations/:id" element={<DestinationDetail/>}/>
+    <Route path="/reviews" element={<Reviews/>}/>
+    <Route path="/admin" element={<Admin/>}/>
+    <Route path="/contacts" element={<Contacts/>}/>
+  
+    </Routes>
+     
     </>
   )
 }
---
+++ src/pages/DestinationDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DestinationDetail() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DestinationDetail
