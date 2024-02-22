7bd7d6f4d2a3002586fb5b23b0124853b66012a6
moreira8125 <your_github_email>
2024-02-22 19:57:10 +0000

    changed icon to link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import logo from '../../public/images/logoGoingCarefree.png'
-import "../styles/navbar.css"
-import {Link, NavLink} from   'react-router-dom'
+import React from "react";
+import logo from "../../public/images/logoGoingCarefree.png";
+import "../styles/navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
- 
   return (
-    <div className='navContainer'>
-      <img src={logo} />
-      <div className='options'>
-      <NavLink className={'navlink'} to={'/'} >Home</NavLink>
-      <NavLink className={'navlink'} to={'/destinations'}>Destinations</NavLink>
-      <NavLink className={'navlink'} to={'/reviews'}>Reviews</NavLink>
-      <NavLink className={'navlink'} to={'/contacts'}>Contacts</NavLink>
+    <div className="navContainer">
+      <Link to={"/"}>
+        <img src={logo} />
+      </Link>
+      <div className="options">
+        <NavLink className={"navlink"} to={"/"}>
+          Home
+        </NavLink>
+        <NavLink className={"navlink"} to={"/destinations"}>
+          Destinations
+        </NavLink>
+        <NavLink className={"navlink"} to={"/reviews"}>
+          Reviews
+        </NavLink>
+        <NavLink className={"navlink"} to={"/contacts"}>
+          Contacts
+        </NavLink>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
