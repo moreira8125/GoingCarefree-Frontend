e92d13eed89f71bcbd2c35ad419c630d855e918b
moreira8125 <your_github_email>
2024-03-07 17:26:08 +0000

    commit so i can pull

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -36,7 +36,7 @@ function Contacts() {
   return (
     <>
       <div
-        className="h-full pt-16 bg-cover bg-center flex justify-center items-center "
+        className="h-full pt-12  bg-cover bg-center flex justify-center items-center "
         style={{
           backgroundImage: `url(${bgImage})`,
           height: "calc(100vh - 100px)",
